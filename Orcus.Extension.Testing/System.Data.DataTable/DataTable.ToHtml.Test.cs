279f22aecfc49cb0e1f0975670f8b785db01f552
Yakup KALEBAŞI <kalebasiyakup@gmail.com>
2018-05-03 21:45:00 +0300

    Genel düzenlemeler yapılıyor.

---
+++ Orcus.Extension.Testing/System.Data.DataTable/DataTable.ToHtml.Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Orcus.Extension.Testing
+{
+    public partial class OrcusExtensionTest
+    {
+        [TestMethod]
+        public void DataTableToHtmlTest()
+        {
+            var dt = DataSource.Products.ToDataTable();
+            var html = dt.ToHtmlTable(true, true);
+
+            Assert.IsNotNull(html);
+        }
+    }
+}
--- Orcus.Extension.Testing/_TestDataSource/DataSource.cs
+++ Orcus.Extension.Testing/_TestDataSource/DataSource.cs
@@ -10,6 +10,7 @@ namespace Orcus.Extension.Testing
         public int? CategoryID { get; set; }
         public Decimal? UnitPrice { get; set; }
         public bool OutOfStock { get; set; }
+        public DateTime StokDateTime { get; set; }
     }
 
     public class Category
@@ -40,12 +41,12 @@ namespace Orcus.Extension.Testing
             {
                 List<Product> prodList = new List<Product>
                 {
-                    new Product { ProductID = 1, ProductName = "Bath Rug, Soft", CategoryID = 1, UnitPrice = 24.5m },
-                    new Product { ProductID = 2, ProductName = "Shower Curtain", CategoryID = 1, UnitPrice = 30.99m },
-                    new Product { ProductID = 3, ProductName = "Soap Dispenser", CategoryID = 1, UnitPrice = 12.4m, OutOfStock = true },
-                    new Product { ProductID = 4, ProductName = "Toilet Tissue", CategoryID = 1, UnitPrice = 15 },
-                    new Product { ProductID = 5, ProductName = "Branket", CategoryID = 2, UnitPrice = 60 },
-                    new Product { ProductID = 6, ProductName = "Mattress Protector", CategoryID = 2, UnitPrice = 30.4m, OutOfStock = true },
+                    new Product { ProductID = 1, ProductName = "Bath Rug, Soft", CategoryID = 1, UnitPrice = 24.5m, StokDateTime = DateTime.Now },
+                    new Product { ProductID = 2, ProductName = "Shower Curtain", CategoryID = 1, UnitPrice = 30.99m, StokDateTime = DateTime.Now.AddDays(1) },
+                    new Product { ProductID = 3, ProductName = "Soap Dispenser", CategoryID = 1, UnitPrice = 12.4m, OutOfStock = true, StokDateTime = DateTime.Now.AddHours(2) },
+                    new Product { ProductID = 4, ProductName = "Toilet Tissue", CategoryID = 1, UnitPrice = 15, StokDateTime = DateTime.Now.AddMilliseconds(34) },
+                    new Product { ProductID = 5, ProductName = "Branket", CategoryID = 2, UnitPrice = 60, StokDateTime = DateTime.Now.AddSeconds(23) },
+                    new Product { ProductID = 6, ProductName = "Mattress Protector", CategoryID = 2, UnitPrice = 30.4m, OutOfStock = true, StokDateTime = DateTime.Now.AddYears(2) },
                     new Product { ProductID = 7, ProductName = "Sheet Set", CategoryID = 2, UnitPrice = 40.69m },
                     new Product { ProductID = 8, ProductName = "Baking Pan", CategoryID = 3, UnitPrice = 10.99m },
                     new Product { ProductID = 9, ProductName = "Coffee Maker", CategoryID = 3, UnitPrice = 49.39m },
